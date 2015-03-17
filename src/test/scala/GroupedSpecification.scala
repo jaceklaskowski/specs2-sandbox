8ce65af574155c91d91774349bda56c79c26fe60
Jacek Laskowski <jacek@japila.pl>
2015-03-17 13:32:47 +0100

    Add example to Grouped

--- src/test/scala/GroupedSpecification.scala
+++ src/test/scala/GroupedSpecification.scala
@@ -7,4 +7,8 @@ class GroupedSpecification extends Specification with Grouped { def is =  s2"""
   first example in second group   ${g2.e1}
   first example in third group    ${g3.e1}
   """
+
+  new g3 {
+    e1 := success
+  }
 }
